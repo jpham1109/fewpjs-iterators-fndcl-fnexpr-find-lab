fc88522475645ce3a2cf6776a2fd8e7ab9828bdd
Jamie H Pham <jpham1109@gmail.com>
2021-03-21 10:52:08 -0400

    Add bang bang, but works without

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 const superbowlWin = (array) => { 
     const record = array.find((record) => record.result === 'W')
-    return record ? record.year : undefined
+    return !!record ? record.year : undefined
 }
